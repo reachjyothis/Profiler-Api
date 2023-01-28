e186c68ad23ab9b77f0f66f488a5d1a3ad141736
Jyothis T S <jyothis.pillai@codeworldwide.com>
2023-01-28 14:01:37 +0000

    Add project files.

---
+++ Profiler-Api/Controllers/PersonalController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Profiler_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonalController : ControllerBase
+    {
+        // GET: api/<PersonalController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PersonalController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonalController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonalController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonalController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
